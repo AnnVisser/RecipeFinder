ebfec8c7dc20d6ff655e36540ec4eb52d7ae8939
Annemarie Visser <annvisser@ziggo.nl>
2023-08-19 16:43:32 +0200

    Search function is done

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -1,14 +1,30 @@
 import { TextInput } from "./ui/TextInput";
 import { useState } from "react";
+import { RecipeList } from "./RecipeList";
+import { data } from "../utils/data";
 
-export const RecipeSearch = () => {
-	const [searchField, setSearchField] = useState("TestRecipe");
+export const RecipeSearch = ({ clickFn }) => {
+	const [searchField, setSearchField] = useState(" ");
+
+	const matchedRecipe = data.hits.filter((recipe) => {
+		return (
+			recipe.recipe.label.toLowerCase().includes(searchField.toLowerCase()) ||
+			recipe.recipe.healthLabels
+				.toString()
+				.toLowerCase()
+				.includes(searchField.toLowerCase())
+		);
+	});
+
+	const handleChange = (event) => {
+		setSearchField(event.target.value);
+	};
 
 	return (
 		<>
 			<label>Search for recipes here:</label>
-			<TextInput />
-			<p>{searchField}</p>
+			<TextInput changeFn={handleChange} />
+			<RecipeList clickFn={clickFn} recipes={matchedRecipe} />
 		</>
 	);
 };
